cf400663033b03460313f1f6f7a811f2ff7fc7c2
leonelcolina <tang.leonel@gmail.com>
2022-06-17 15:12:35 -0400

    completed

--- execution_time.rb
+++ execution_time.rb
@@ -16,16 +16,40 @@ end
 # list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(list)
 
+# def largest_contiguous_subsum(list)
+
+#     array = []
+#     list.each.with_index do |ele1, idx1| #O(n)
+#         list.each.with_index do |ele2, idx2| #O(n)
+#             array << list[idx1..idx2].sum if idx2 > idx1 #O(n)
+#         end
+#     end
+#     array.max# O(n)
+
+#     #O(n + n^3)
+# end
+
+# list = [2, 3, -6, 7, -6, 7]
+# p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+
+# [2], [2,3], [2,3,-6], [2,3,-6,7]
+# [0], [0..1], [0..2], [0..3]
+
+
 def largest_contiguous_subsum(list)
 
-    array = []
-    list.each.with_index do |ele1, idx1|
-        list.each.with_index do |ele2, idx2|
-            array << list[idx1..idx2].sum if idx2 > idx1
-        end
+max = list.first
+current = list.first #if current > max, current = max
+#if current is less than 0, we set current == 0
+    list.drop(1).each do |ele|
+        current += ele
+        max = current if current > max
+        current = 0 if current.negative
     end
-    array.max
+    max
 end
 
 list = [2, 3, -6, 7, -6, 7]
 p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+
+        
