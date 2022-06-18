8894b277b680b91f7ff2ddcfbe605d4d8d810eda
Anthony Chao <anthonyschaowder@gmail.com>
2022-06-18 19:31:51 -0400

    finished anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -3,7 +3,7 @@ def first_anagram?(string)
     arr.map! { |word| word.join("")}
 end
 
-p first_anagram?("leonel").length
+# p first_anagram?("leonel").length
 
 
 def second_anagram?(str1, str2)
@@ -21,5 +21,41 @@ def second_anagram?(str1, str2)
     word.empty?
 end
 
-p second_anagram?("hello", "helmo")
+# p second_anagram?("hello", "helmo")
 
+def third_anagram?(str1, str2)
+    #O(n) because the built-in split method is O(n) - it has to do more work as the input scales
+    str1.split("").sort == str2.split("").sort
+
+end
+
+# p third_anagram?("hello", "helmo")
+# p third_anagram?("woooof", "foooow")
+
+def fourth_anagram?(str1, str2)
+    #O(n) because both iterations are not nested
+    count1 = Hash.new(0)
+    str1.each_char { |ele| count1[ele] += 1 }
+
+    count2 = Hash.new(0)
+    str2.each_char { |ele| count2[ele] += 1 }
+
+    count1 == count2
+end
+
+# p fourth_anagram?("hello", "helmo")
+# p fourth_anagram?("woooof", "foooow")
+
+def fifth_anagram?(str1, str2)
+
+    count1 = Hash.new(0)
+    str1.each_char { |ele| count1[ele] += 1 }
+
+    str2.each_char { |ele| count1[ele] -= 1 }
+
+    count1.all? { |k, v| v == 0 }
+
+end
+
+p fifth_anagram?("hello", "helmo")
+p fifth_anagram?("woooof", "foooow")
